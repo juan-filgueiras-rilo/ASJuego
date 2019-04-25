82e166bcf4b25f09dcb01268995aa4fa338cab17
Juan Falcon <juan.falcon.abal@udc.es>
2019-04-25 10:15:45 +0200

    añadido pattern matching a peer

--- lib/peer.ex
+++ lib/peer.ex
@@ -1,25 +1,29 @@
 defmodule Peer do
 
 
-
-
 def registrar() do
-  case GenServer.call({:super,:"super@192.168.43.142"}, {:registrar,Node.self()}, 5000) do
-    :ok->IO.puts("Registro satisfatorio")
-    _-> IO.puts("Fallo en el registro, intentelo de nuevo")
-  end
-
+	mensaje_registrar(GenServer.call({:super,:"super@192.168.43.142"}, {:registrar,Node.self()}, 5000))
 end
 
+def mensaje_registrar(:ok) do
+	IO.puts("Registro satisfatorio")
+end
+
+def mensaje_registrar(_) do
+	IO.puts("Fallo en el registro, intentelo de nuevo")
+end
 
 def buscar_rival() do
-
-  case GenServer.call({:super,:"super@192.168.43.142"}, :pedir_lista, 5000) do
-    {:ok,lista}->Enum.random(lista)  #Seleciono aleatoriamente un dos peers que me devolve o superPeer
-     _-> IO.puts("Fallo en la conexion con el superpeer, intentelo de nuevo")
-  end
-
+  resultado_buscar(GenServer.call({:super,:"super@192.168.43.142"}, :pedir_lista, 5000))
 end
+
+def resultado_buscar(:ok, lista) do
+	Enum.random(lista)
+end
+
+def resultado_buscar(_) do
+	IO.puts("Fallo en la conexion con el superpeer, intentelo de nuevo")
+end 
 
 
 def conectarse(pid) do
