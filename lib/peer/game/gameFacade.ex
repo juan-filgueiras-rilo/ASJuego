ed5a5c4fb4c0509bcdf6091a0179836edb555105
Marco Martínez Sánchez <marco.msanchez@udc.es>
2019-04-25 21:02:18 +0200

    Primera implementacion completa (sin tener un proceso que lo ejecute) de la logica de juego.

--- lib/peer/game/gameFacade.ex
+++ lib/peer/game/gameFacade.ex
@@ -4,14 +4,7 @@ defmodule GameFacade do
 # ESTA ZONA CONTIENE METODOS LOCALES
 #------------------------------------
 
-    defp generarMundo(fileName)
-    do
-        # Llamada por load cuando no hay disponible información del mundo.
-        # Guarda automaticamente el mundo para posteriores usos.
-        :todo
-    end
-
-    def constructor(fileName)
+    def constructor(fileName, pidCallback)
         when is_binary(fileName)
     do
         # Cargamos la información del jugador, de las clases y sus hechizos y
@@ -27,108 +20,126 @@ defmodule GameFacade do
     end
 
 
-    #
-    # Esta zona se refiere al proceso de linkado de mundos. Es el que sigue:
-    # 
-    # 1) Se inicia el linkado llamando al metodo "startLinkWorld" en uno de los peers.
-    #    Esta funcion nos devuelve una propuesta de sectores para ser enlazados, y un
-    #    nombre que se le mostrará al otro peer para referirse a este mundo.
-    #
-    # 2) Se finaliza el proceso de linkado llamando al metodo "linkWorld" en el otro peer.
-    #    Esta función recibe la información generada por el otro peer junto con el atomo
-    #    :generate , y enlaza los sectores localmente  devolviendo los datos de los
-    #    enlaces establecidos entre los mundos.
-    #
-    # 3) En el peer inicial se llama al metodo "linkWorld" con el atomo :link.
-    #    Este metodo ya recibe los enlaces establecidos por el peer entre los sectores
-    #    de ambos peers, y los establece localmente.
-    #
-    # 4) Si un peer quisiera desconectar un mundo, debería llamar a "unlinkWorld"
-    #    pasando el nombre del enlace. Este metodo solo desconecta los enlaces de un
-    #    lado del peer. Es responsabilidad del componente correspondiente (red o IU)
-    #    desconectar el otro lado (si es que sigue vivo).
-    #
-
-    def startLinkWorld(gameState)
-    do
-        # Comienza el proceso de conexión de nuestro mundo. Aquí devolvemos el nombre
-        # y sectores que proponemos para el enlace.
-    end
-
-    def linkWorld(gameState, {:generate, nombre, sectores})
-    do
-        # Enlaza nuestro mundo. Aquí nos están proponiendo
-        # ya unos sectores, así que devolvemos los enlaces establecidos entre sus sectores
-        # y los nuestros.
-        :todo
-    end
-
-    def linkWorld(gameState, {:link, enlaces})
-    do
-        :todo
-    end
-
-    def unlinkWorld(gameState, nombre)
-    do
-        :todo
-        # Desenlaza el mundo representado por nombre de nuestro mundo
-    end
-
-
-#--------------------------------------------------------
-# ESTA ZONA PUEDE USARSE POR OTROS PEERS EN NUESTRO MUNDO
-#--------------------------------------------------------
-
-
-    def getJugador(gameState, id)
+    def getJugador(gameState)
     do
         #Devolvemos una referencia al jugador, aunque es de solo lectura
         case gameState do
-            {jugadores, mundo, clases, combates} -> Enum.find(jugadores, fn {idPriv, idPub, estado, jugador} -> idPub = id end)
+            {callBackIU, clases, jugador, datosEnemigo} -> jugador
             _ -> :error
         end
     end
 
-    def login(gameState, player, sector, pidCallback)
+    def iniciarCombate(gameState, :synCombate)
     do
-        # DEVOLVER (ID_PUB, ID_PRIV) DEL JUGADOR GENERADO AL CONECTARSE. En el sector
-        # se almacena el id_pub del jugador
-        
-        # Ademas, el pidCallback es a donde se enviaran las respuestas a eventos
-        # (como combates que se inician, resultados del combate, o enlaces establecidos).
-        :todo
+        #Comenzamos el proceso de iniciar combate
+        case gameState do
+            {callBackIU, clases, jugador, {:fueraCombate}} -> 
+                estado = {callBackIU, clases, jugador, {:estableciendo}};
+                {:ok, self(), jugador}
+            _ -> :error
+        end
     end
 
-    def logout(gameState, idPriv)
+    def iniciarCombate(gameState, :ack1Combate, pid, datosEnemigo)
     do
-        # Desconecta un jugador del mundo.
-        :todo
+        # Inicia un combate recibiendo el pid para comunicar el estado,
+        # y devuelve mi pid para poder recibir informacion del combate.
+        case gameState do
+            {callBackIU, clases, jugador, {:fueraCombate}} -> 
+                estado = {callBackIU, clases, jugador, {:combate, pid, datosEnemigo, [], []}};
+                {:ok, self(), jugador}
+            _ -> :error
+        end
     end
 
-    def moverse(gameState, idPriv, sector)
+    def iniciarCombate(gameState, :ack2Combate, pid, datosEnemigo)
     do
-        # Te mueve a otro sector (dentro de los gestionados por este peer, si no 
-        # habria que desloguearse aquí, y loguearse allí).
-        :todo
+        # Inicia un combate pasando el pid del peer2 para poder comunicar el
+        # estado.
+        case gameState do
+            {callBackIU, clases, jugador, {:estableciendo}} -> 
+                estado = {callBackIU, clases, jugador, {:combate, pid, datosEnemigo, [], []}};
+                :ok
+            _ -> :error
+        end
     end
 
-    def atacar(gameState, idPriv, idPub)
-    do
-        # Ataca (si es posible) al jugador identificado por idPub.
-        :todo
-    end
 
-    def retirarse(gameState, idPriv)
+
+    def retirarse(gameState)
     do
         # Te retiras del combate
-        :todo
+        case gameState do
+            {callBackIU, clases, jugador, {:combate, pid, datosEnemigo, efectosPropios, efectosEnemigo}} -> 
+                estado = {callBackIU, clases, jugador, {:fueraCombate}};
+                :ok
+            _ -> :error
+        end
     end
 
-    def usarHechizo(gameState, idPriv, hechizo)
+    defp getHechizoDeEfecto({duracionRestante, hechizo})
     do
-        # Usas un hechizo (solo funcionara si es tu turno, estas en combate y el idPriv es valido)
-        
-        :todo
+        hechizo
+    end
+
+    defp getHechizosDeEfectos([{duracionRestante, hechizo} | efectos], salida)
+    do
+        getHechizosDeEfectos(efectos, [hechizo | salida])
+    end
+
+    defp getHechizosDeEfectos([], salida)
+    do
+        salida
+    end
+
+    defp reducirDuracionesEfectos([], nuevaLista)
+    do
+        nuevaLista
+    end
+
+    defp reducirDuracionesEfectos([{duracionRestante, hechizo} | hechizos], nuevaLista)
+        when duracionRestante > 1
+    do
+        reducirDuracionesEfectos(hechizos, [{duracionRestante - 1, hechizo} | nuevaLista])
+    end
+
+    defp reducirDuracionesEfectos([{1, hechizo} | hechizos], nuevaLista)
+    do
+        reducirDuracionesEfectos(hechizos, nuevaLista)
+    end
+
+    def usarHechizoPropio(gameState, hechizo)
+    do
+        # Usas un hechizo propio, y actualiza el estado aplicando los efectos del enemigo.
+        case gameState do
+            {callBackIU, clases, jugador, {:combate, pid, datosEnemigo, efectosPropios, efectosEnemigo}} -> 
+                
+                efectosEnemigo = [{Hechizo.getDuracion(hechizo), hechizo} | efectosEnemigo];
+                {jugador, datosEnemigo} = Jugador.aplicarHechizos(jugador, getHechizosDeEfectos(efectosEnemigo, []), datosEnemigo);
+                
+                efectosEnemigo = reducirDuracionesEfectos(efectosEnemigo, []);
+                
+                estado = {callBackIU, clases, jugador, {:combate, pid, datosEnemigo, efectosPropios, efectosEnemigo}};
+                
+            _ -> :error
+        end
+    end
+
+    def usarHechizoRemoto(gameState, hechizo)
+    do
+        # Usas un hechizo propio, y actualiza el estado aplicando los efectos del enemigo.
+        case gameState do
+            {callBackIU, clases, jugador, {:combate, pid, datosEnemigo, efectosPropios, efectosEnemigo}} -> 
+                
+                efectosPropios = [{Hechizo.getDuracion(hechizo), hechizo} | efectosPropios];
+                {datosEnemigo, jugador} = Jugador.aplicarHechizos(datosEnemigo, getHechizosDeEfectos(efectosPropios, []), jugador);
+                
+                efectosPropios = reducirDuracionesEfectos(efectosPropios, []);
+                
+                estado = {callBackIU, clases, jugador, {:combate, pid, datosEnemigo, efectosPropios, efectosEnemigo}};
+                
+            _ -> :error
+        end
     end
 
 end
--- lib/peer/game/jugador.ex
+++ lib/peer/game/jugador.ex
@@ -39,4 +39,27 @@ defmodule Jugador do
     do
         Clase.getHechizosDisponibles(clase, nivel)
     end
+
+    def aplicarHechizo({nombrePropio, nivelPropio, clasePropia, vidaPropia}, hechizo, {nombreEnemigo, nivelEnemigo, claseEnemigo, vidaEnemigo})
+    do
+        # Utilizo un hechizo y devuelvo el par con los datos mios y del enemigo actualizados.
+        fuerza = Hechizo.getFuerza(hechizo, nivelEnemigo);
+        case Hechizo.getTipo(hechizo) do
+            "DMG" -> {{nombrePropio, nivelPropio, clasePropia, vidaPropia}, {nombreEnemigo, nivelEnemigo, claseEnemigo, vidaEnemigo - fuerza}}
+            "CUR" -> {{nombrePropio, nivelPropio, clasePropia, vidaPropia + fuerza}, {nombreEnemigo, nivelEnemigo, claseEnemigo, vidaEnemigo}}
+            "ROB" -> {{nombrePropio, nivelPropio, clasePropia, vidaPropia + fuerza}, {nombreEnemigo, nivelEnemigo, claseEnemigo, vidaEnemigo - fuerza}}
+        end  
+    end
+
+    def aplicarHechizos(jugador, [], enemigo)
+    do
+        {jugador, enemigo}
+    end
+
+    def aplicarHechizos(jugador, hechizos, enemigo)
+        when is_list(hechizos)
+    do
+        {jugador, enemigo} = aplicarHechizos(jugador, tl(hechizos), enemigo);
+        aplicarHechizo(jugador, hd(hechizos), enemigo)
+    end
 end
