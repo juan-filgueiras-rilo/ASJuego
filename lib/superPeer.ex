f2ac22baf2a5f787027d355f613fa571ec64580e
ishwara Coello <ishwara.coello@udc.es>
2019-05-02 22:08:47 +0200

    metodo para borrar peer registrado en superPeer

--- lib/superPeer.ex
+++ lib/superPeer.ex
@@ -16,12 +16,15 @@ defmodule SuperPeer do
   def handle_call({:registrar, node}, from, {list, counter}) do
     IO.inspect(from)
 
-    {:reply, {:ok, counter}, {[{node,counter} | list], counter + 1}}
+    {:reply, {:ok, counter}, {[{node, counter} | list], counter + 1}}
   end
 
   def handle_call({:pedir_lista, nodeOrigin}, _from, {list, counter}) do
-
-filterdList = Enum.filter(list, fn {node,_} -> node != nodeOrigin end)
+    filterdList = Enum.filter(list, fn {node, _} -> node != nodeOrigin end)
     {:reply, {:ok, filterdList}, {list, counter}}
+  end
+
+  def handle_call({:delete_node, nodeOrigin}, _from, {list, counter}) do
+    {:reply, {:ok}, {Enum.filter(list, fn node -> node != nodeOrigin end)}}
   end
 end
