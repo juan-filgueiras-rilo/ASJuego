18fa9e3dedb209c59e7cd8256492c272dabb7c4b
Marco Martínez Sánchez <marco.msanchez@udc.es>
2019-05-07 12:24:19 +0200

    Comenzada implementacion de pruebas del modulo de juego

---
+++ test/peer/game/gameFacade_test.exs
@@ -0,0 +1,79 @@
+defmodule GameFacadeTest do
+    use ExUnit.Case
+    doctest GameFacade
+  
+    test "CreatePlayer" do
+        {:ok, pid} = GameFacade.iniciar("./data/GameData.json", self());
+        clases = GameFacade.listarClases(pid);
+        jugador = Jugador.constructor("prueba", 1, hd(clases));
+        GameFacade.crearJugador(pid, jugador);
+        assert (jugador == GameFacade.obtenerJugador(pid))
+    end
+
+    test "MultipleLoadsError" do
+        {:ok, pid} = GameFacade.iniciar("./data/GameData.json", self());
+        clases = GameFacade.listarClases(pid);
+        jugador = Jugador.constructor("prueba", 1, hd(clases));
+        GameFacade.crearJugador(pid, jugador);
+        assert (GameFacade.crearJugador(pid, jugador) == :estadoInvalido)
+    end
+
+    test "ListSpellsOutOfCombatError" do
+        {:ok, pid} = GameFacade.iniciar("./data/GameData.json", self());
+        clases = GameFacade.listarClases(pid);
+        jugador = Jugador.constructor("prueba", 1, hd(clases));
+        GameFacade.crearJugador(pid, jugador);
+        assert (GameFacade.getHechizosDisponibles(pid) == :estadoInvalido)
+    end
+
+    test "UseSpellTwotimesError" do
+        {:ok, pid} = GameFacade.iniciar("./data/GameData.json", self());
+        clases = GameFacade.listarClases(pid);
+        jugador = Jugador.constructor("prueba", 1, hd(clases));
+        GameFacade.crearJugador(pid, jugador);
+        GameFacade.ackCombate(pid, self(), jugador);
+        hechizos = GameFacade.getHechizosDisponibles(pid);
+        GameFacade.usarHechizoPropio(pid, hd(hechizos))
+        assert (GameFacade.usarHechizoPropio(pid, hd(tl(hechizos))) == :turnoInvalido)
+    end
+
+    test "UseSpellOutOfCombatError" do
+        {:ok, pid} = GameFacade.iniciar("./data/GameData.json", self());
+        clases = GameFacade.listarClases(pid);
+        jugador = Jugador.constructor("prueba", 1, hd(clases));
+        GameFacade.crearJugador(pid, jugador);
+        assert (GameFacade.usarHechizoPropio(pid, {:ok}) == :estadoInvalido)
+    end
+
+    test "UseLocalSpell" do
+        {:ok, pid} = GameFacade.iniciar("./data/GameData.json", self());
+        clases = GameFacade.listarClases(pid);
+        jugador = Jugador.constructor("prueba", 1, hd(clases));
+        GameFacade.crearJugador(pid, jugador);
+        GameFacade.ackCombate(pid, self(), jugador);
+        hechizos = GameFacade.getHechizosDisponibles(pid);
+        ori = GameFacade.obtenerEnemigo(pid);
+        GameFacade.usarHechizoPropio(pid, hd(hechizos));
+        result = GameFacade.obtenerEnemigo(pid);
+        
+        assert Jugador.getVida(ori) > Jugador.getVida(result)
+    end
+
+    test "UseRemoteSpell" do
+        {:ok, pid} = GameFacade.iniciar("./data/GameData.json", self());
+        clases = GameFacade.listarClases(pid);
+        jugador = Jugador.constructor("prueba", 1, hd(clases));
+        GameFacade.crearJugador(pid, jugador);
+        GameFacade.ackCombate(pid, self(), jugador);
+        hechizos = GameFacade.getHechizosDisponibles(pid);
+        
+        ori = GameFacade.obtenerJugador(pid);
+        GameFacade.usarHechizoPropio(pid, hd(hechizos));
+        GameFacade.usarHechizoRemoto(pid, hd(hechizos));
+        result = GameFacade.obtenerJugador(pid);
+        
+        assert Jugador.getVida(ori) > Jugador.getVida(result)
+    end
+
+end
+
