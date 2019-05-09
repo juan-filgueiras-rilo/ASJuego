10852be5c9a87214c87fc3d6eb041c6745d7cdc0
Marco Martínez Sánchez <marco.msanchez@udc.es>
2019-05-09 20:15:05 +0200

    Pa ti el script

---
+++ lanzarContainerDocker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ "$#" -eq 0 ]; then
+    sudo docker run \
+    -it \
+    --rm \
+    -v "/home/marco/FIC/Practicas/Tercero/Segundo Cuatrimestre/AS/practica":/practica \
+    -v "/home/marco/.mix":/root/.mix \
+    -w /practica elixir \
+    iex -S mix
+fi
+if [ "$#" -eq 1 ]; then
+    sudo docker run \
+    -it \
+    --rm \
+    -v "/home/marco/FIC/Practicas/Tercero/Segundo Cuatrimestre/AS/practica":/practica \
+    -v "/home/marco/.mix":/root/.mix \
+    -w /practica elixir \
+    iex -S mix run $1
+fi
+
+    
