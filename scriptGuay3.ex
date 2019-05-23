c873ca0a1ccff79ddc9c0e52d56f2e109f17deb1
Juan Luis Filgueiras Rilo <juan.filgueiras.rilo@udc.es>
2019-05-23 16:41:30 +0200

    reorganizacion

--- scriptGuay3.ex
+++
@@ -1,4 +0,0 @@
-pidDebug = DebugGuay.debug();
-{:ok, pidJuego} = GameFacade.iniciar(pidDebug, "./data/GameData.json");
-GameFacade.cargar(pidJuego, "./data/PlayerData.json");
-GameFacade.ackCombate()
